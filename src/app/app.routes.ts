f8dd2d0439bd93f6d2632cac2d12818fdcf02b40
shiv159 <zaynster007@gmail.com>
2024-07-14 19:18:42 +0530

    partical three.js

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { ThreeComponent } from './components/three/three.component';
 import { LookatComponent } from './components/lookat/lookat.component';
 import { TextureComponent } from './components/texture/texture.component';
+import { ParticalsComponent } from './components/particals/particals.component';
 
 export const routes: Routes = [
   { path: 'home', component: ThreeComponent },
   { path: 'lookat', component: LookatComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'texture', component: TextureComponent },
+  { path: 'particals', component: ParticalsComponent },
 ];

7fbefc0baaa5a5baee71ed00dd51c1ce58a3e0fb
shiv159 <zaynster007@gmail.com>
2024-07-16 01:49:46 +0530

    3d model added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { ThreeComponent } from './components/three/three.component';
 import { LookatComponent } from './components/lookat/lookat.component';
 import { TextureComponent } from './components/texture/texture.component';
 import { ParticalsComponent } from './components/particals/particals.component';
+import { ModelsComponent } from './components/models/models.component';
 
 export const routes: Routes = [
   { path: 'home', component: ThreeComponent },
@@ -10,4 +11,5 @@ export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'texture', component: TextureComponent },
   { path: 'particals', component: ParticalsComponent },
+  { path: 'model', component: ModelsComponent },
 ];
---
+++ src/app/components/models/models.component.ts
@@ -0,0 +1,92 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import * as dat from 'dat.gui';
+@Component({
+  selector: 'app-models',
+  standalone: true,
+  imports: [],
+  templateUrl: './models.component.html',
+  styleUrl: './models.component.css',
+})
+export class ModelsComponent {
+  @ViewChild('canvas', { static: true })
+  canvasRef!: ElementRef<HTMLCanvasElement>;
+
+  ngAfterViewInit(): void {
+    const canvas = this.canvasRef.nativeElement;
+
+    //create a scene----------------------------------------
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color('grey');
+    const axesHelper = new THREE.AxesHelper(5); // Specify the length of the axes
+    scene.add(axesHelper);
+    const light = new THREE.AmbientLight(); // soft white light
+    scene.add(light);
+    //load the model----------------------------------------
+    const loader = new GLTFLoader();
+
+    loader.load(
+      // resource URL
+      '../../../assets/mustang/scene.gltf',
+      // called when the resource is loaded
+      function (gltf) {
+        gltf.scene.scale.set(1, 1, 1);
+        scene.add(gltf.scene);
+        //debugger dat.ui-------------------------------------------
+
+        gltf.animations; // Array<THREE.AnimationClip>
+        gltf.scene; // THREE.Group
+        gltf.scenes; // Array<THREE.Group>
+        gltf.cameras; // Array<THREE.Camera>
+        console.log(gltf.asset); // Object
+      },
+      // called while loading is progressing
+      function (xhr) {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+      },
+      // called when loading has errors
+      function (error) {
+        console.log('An error happened');
+      }
+    );
+    //debugger dat.ui-------------------------------------------
+
+    //create a camera------------------------------------------
+    const aspect = {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      1,
+      1000
+    );
+    camera.position.z = 2;
+
+    //create a renderer-------------------------------------------
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvas,
+      antialias: true,
+    }); //add canvas to renderer
+    renderer.setSize(window.innerWidth, window.innerHeight); //set size of renderer
+
+    //orbit controls-----------------------------------------------
+    const orbitcontrols = new OrbitControls(camera, canvas);
+    orbitcontrols.dampingFactor = 0.2;
+
+    //create a animate function------------------------------------
+    const clock = new THREE.Clock();
+    const animate = () => {
+      const elapsedTime = clock.getElapsedTime();
+      requestAnimationFrame(animate);
+      //  cube.rotation.y += elapsedTime * 0.0001;
+      renderer.render(scene, camera); //render the scene
+    };
+    animate();
+  }
+}
